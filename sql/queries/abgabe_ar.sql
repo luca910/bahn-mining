e83381857ad2c0f4d94cf0c669131d544c15b4dd
lucakrawczyk <git@lucakrawczyk.de>
2024-11-28 09:46:25 +0100

    Queries für Abgabedaten

---
+++ sql/queries/abgabe_ar.sql
@@ -0,0 +1,3 @@
+SELECT t.*
+FROM bahn.ar_superquery t
+WHERE plannedArrival >= '2024-10-26 00:00:00' AND plannedArrival <  '2024-11-26 23:59:59'
---
+++ sql/queries/abgabe_dp.sql
@@ -0,0 +1,3 @@
+SELECT t.*
+FROM bahn.dp_superquery t
+WHERE plannedDeparture >= '2024-10-26 00:00:00' AND plannedDeparture <  '2024-11-26 23:59:59'
